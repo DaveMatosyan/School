80c367e5eb9dfba0e39237229aa90f56a70069f2
DaveMatosyan <dvdmatosyan@gmail.com>
2022-08-24 15:25:00 +0400

    adding modal

--- School Project/wwwroot/js/site.js
+++ School Project/wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const teacherModalButton = document.getElementById('teacherModalButton');
+const teacherModal = document.getElementById('teacherModal');
+const closeTeacherModal = document.querySelectorAll('.closeTeacherModal');
+console.log(closeTeacherModal);
+teacherModalButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    teacherModal.classList.add('show-modal');
+})
+closeTeacherModal.forEach(item => {
+    item.addEventListener('click', (event) => {
+        event.preventDefault();
+        teacherModal.classList.remove('show-modal');
+    })
+})
