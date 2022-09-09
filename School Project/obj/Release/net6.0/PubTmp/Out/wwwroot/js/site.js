84101e4c6e76d5eb6a8089c346a443b27b8a6a6a
DaveMatosyan <dvdmatosyan@gmail.com>
2022-09-09 15:20:50 +0400

    test

---
+++ School Project/obj/Release/net6.0/PubTmp/Out/wwwroot/js/site.js
@@ -0,0 +1,33 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+const teacherModalButton = document.querySelectorAll('.teacherModalButton');
+const teacherModal = document.getElementById('teacherModal');
+const closeTeacherModal = document.querySelectorAll('.closeTeacherModal');
+console.log(closeTeacherModal);
+
+teacherModalButton.forEach(item => {
+    item.addEventListener('click', (event) => {
+        event.preventDefault();
+        teacherModal.classList.add('show-modal');
+    })
+})
+
+closeTeacherModal.forEach(item => {
+    item.addEventListener('click', (event) => {
+        event.preventDefault();
+        teacherModal.classList.remove('show-modal');
+    })
+})
+
+
+$(document).ready(function () {
+
+    $('.sidebarCollapse').on('click', function () {
+        $('.header').toggleClass('active');
+        $('.sidebarCollapse').toggleClass('closed');
+    });
+
+});
